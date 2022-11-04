d760878d1659d826c53537774ac69c31950da41a
eunjooJeon <113004597+eunjooJeon@users.noreply.github.com>
2022-11-04 14:35:25 +0900

    Add files via upload

---
+++ final_hobbyplanet_dummy.sql
@@ -0,0 +1,56 @@
+Insert into USERTABLE (U_ID,U_PWD,U_NICKNAME,U_NAME,U_AGE_Y,U_AGE_M,U_AGE_D,U_EMAIL,U_TEL1,U_TEL2,U_TEL3,U_ADDR1,U_ADDR2,U_ADDR3,U_GENDER,U_REGDATE) values ('aaaaaa','aaaaaa','나는에이','김에이',2000,2,4,'aaa@aaa.com','010',1111,1111,'부산광역시', '부산진구','1','여성',to_date('22/09/28','RR/MM/DD'));
+Insert into USERTABLE (U_ID,U_PWD,U_NICKNAME,U_NAME,U_AGE_Y,U_AGE_M,U_AGE_D,U_EMAIL,U_TEL1,U_TEL2,U_TEL3,U_ADDR1,U_ADDR2,U_ADDR3,U_GENDER,U_REGDATE) values ('bbbbbb','bbbbbb','나는비비','박비비',1997,7,13,'bba@abc.com','010',2222,2222,'부산광역시', '사하구',' ','남성',to_date('22/09/28','RR/MM/DD'));
+Insert into USERTABLE (U_ID,U_PWD,U_NICKNAME,U_NAME,U_AGE_Y,U_AGE_M,U_AGE_D,U_EMAIL,U_TEL1,U_TEL2,U_TEL3,U_ADDR1,U_ADDR2,U_ADDR3,U_GENDER,U_REGDATE) values ('ababab','aaaaaa','에이비님','조에이비',1996,12,30,'abab@abb.com','010',2222,1111,'서울특별시', '동작구',' ','남성',to_date('22/09/28','RR/MM/DD'));
+Insert into USERTABLE (U_ID,U_PWD,U_NICKNAME,U_NAME,U_AGE_Y,U_AGE_M,U_AGE_D,U_EMAIL,U_TEL1,U_TEL2,U_TEL3,U_ADDR1,U_ADDR2,U_ADDR3,U_GENDER,U_REGDATE) values ('cccccc','cccccc','씨씨는씨씨','수박씨',1999,10,24,'ccac@cccc.com','010',1313,1313,'부산광역시', '동래구',' ','여성',to_date('22/09/28','RR/MM/DD'));
+Insert into USERTABLE (U_ID,U_PWD,U_NICKNAME,U_NAME,U_AGE_Y,U_AGE_M,U_AGE_D,U_EMAIL,U_TEL1,U_TEL2,U_TEL3,U_ADDR1,U_ADDR2,U_ADDR3,U_GENDER,U_REGDATE) values ('eeeeee','eeeeee','이에이승','이이승',1978,10,16,'eeae@eea.com','010',1234,5678,'부산광역시', '동구',' ','남성',to_date('22/09/28','RR/MM/DD'));
+Insert into USERTABLE (U_ID,U_PWD,U_NICKNAME,U_NAME,U_AGE_Y,U_AGE_M,U_AGE_D,U_EMAIL,U_TEL1,U_TEL2,U_TEL3,U_ADDR1,U_ADDR2,U_ADDR3,U_GENDER,U_REGDATE) values ('qqqqqq','qqqqqq','큐큐따라큐큐','따라큐',1992,11,29,'qqba@qqba.com','010',1234,1234,'서울특별시', '관악구',' ','여성',to_date('22/09/28','RR/MM/DD'));
+Insert into USERTABLE (U_ID,U_PWD,U_NICKNAME,U_NAME,U_AGE_Y,U_AGE_M,U_AGE_D,U_EMAIL,U_TEL1,U_TEL2,U_TEL3,U_ADDR1,U_ADDR2,U_ADDR3,U_GENDER,U_REGDATE) values ('pppppp','pppppp','피카피카','피카츄',1990,10,5,'pika@pika.net','010',1212,1234,'부산광역시', '부산진구',' ','남성',to_date('22/09/28','RR/MM/DD'));
+Insert into USERTABLE (U_ID,U_PWD,U_NICKNAME,U_NAME,U_AGE_Y,U_AGE_M,U_AGE_D,U_EMAIL,U_TEL1,U_TEL2,U_TEL3,U_ADDR1,U_ADDR2,U_ADDR3,U_GENDER,U_REGDATE) values ('wwwwww','wwwwww','더블유유','유유유',1999,10,10,'woww@wow.net','010',9877,7899,'서울특별시', '구로구',' ','여성',to_date('22/09/28','RR/MM/DD'));
+Insert into USERTABLE (U_ID,U_PWD,U_NICKNAME,U_NAME,U_AGE_Y,U_AGE_M,U_AGE_D,U_EMAIL,U_TEL1,U_TEL2,U_TEL3,U_ADDR1,U_ADDR2,U_ADDR3,U_GENDER,U_REGDATE) values ('mmmmma','mmmmmm','엠에이엠','엠에이',1993,7,20,'mama@mama.com','010',4567,1233,'대구광역시', '달서구',' ','여성',to_date('22/09/28','RR/MM/DD'));
+Insert into USERTABLE (U_ID,U_PWD,U_NICKNAME,U_NAME,U_AGE_Y,U_AGE_M,U_AGE_D,U_EMAIL,U_TEL1,U_TEL2,U_TEL3,U_ADDR1,U_ADDR2,U_ADDR3,U_GENDER,U_REGDATE) values ('eeeeev','eeeeee','이브이님','이브이',1995,7,28,'eeev@eeevv.net','010',3456,6543,'경기도', '수원시',' ','여성',to_date('22/09/28','RR/MM/DD'));
+Insert into USERTABLE (U_ID,U_PWD,U_NICKNAME,U_NAME,U_AGE_Y,U_AGE_M,U_AGE_D,U_EMAIL,U_TEL1,U_TEL2,U_TEL3,U_ADDR1,U_ADDR2,U_ADDR3,U_GENDER,U_REGDATE) values ('nannan','nnnnnn','난맛있어','이영희',2001,12,4,'nan@nan.net','010',7878,7878,'전라북도', '남원시',' ','남성',to_date('22/09/28','RR/MM/DD'));
+Insert into USERTABLE (U_ID,U_PWD,U_NICKNAME,U_NAME,U_AGE_Y,U_AGE_M,U_AGE_D,U_EMAIL,U_TEL1,U_TEL2,U_TEL3,U_ADDR1,U_ADDR2,U_ADDR3,U_GENDER,U_REGDATE) values ('ooaooo','oooooo','오늘내일','오오늘',1986,10,11,'today@abc.com','010',4567,3211,'부산광역시', '부산진구',' ','여성',to_date('22/09/28','RR/MM/DD'));
+Insert into USERTABLE (U_ID,U_PWD,U_NICKNAME,U_NAME,U_AGE_Y,U_AGE_M,U_AGE_D,U_EMAIL,U_TEL1,U_TEL2,U_TEL3,U_ADDR1,U_ADDR2,U_ADDR3,U_GENDER,U_REGDATE) values ('uuubuu','uuuuuu','유비유비','유비',1983,9,27,'ubub@ddd.com','010',9876,9876,'부산광역시', '중구',' ','남성',to_date('22/09/28','RR/MM/DD'));
+Insert into USERTABLE (U_ID,U_PWD,U_NICKNAME,U_NAME,U_AGE_Y,U_AGE_M,U_AGE_D,U_EMAIL,U_TEL1,U_TEL2,U_TEL3,U_ADDR1,U_ADDR2,U_ADDR3,U_GENDER,U_REGDATE) values ('rrrrrr','rrrrrr','알루미늄','알루미',1968,4,21,'baba@baba.com','010',4567,4567,'대구광역시','대구',' ','남성',to_date('22/09/29','RR/MM/DD'));
+Insert into USERTABLE (U_ID,U_PWD,U_NICKNAME,U_NAME,U_AGE_Y,U_AGE_M,U_AGE_D,U_EMAIL,U_TEL1,U_TEL2,U_TEL3,U_ADDR1,U_ADDR2,U_ADDR3,U_GENDER,U_REGDATE) values ('tttttt','tttttt','티티티셔츠','티셔츠',1989,6,30,'tshirt@ttss.net','010',6543,6543,'전라남도','대구',' ','여성',to_date('22/09/29','RR/MM/DD'));
+Insert into USERTABLE (U_ID,U_PWD,U_NICKNAME,U_NAME,U_AGE_Y,U_AGE_M,U_AGE_D,U_EMAIL,U_TEL1,U_TEL2,U_TEL3,U_ADDR1,U_ADDR2,U_ADDR3,U_GENDER,U_REGDATE) values ('yyybbb','yyyyyy','와이와이비','와이비',1996,11,23,'ybyb@ybyb.com','010',1234,1212,'광주광역시','대구',' ','남성',to_date('22/09/29','RR/MM/DD'));
+Insert into USERTABLE (U_ID,U_PWD,U_NICKNAME,U_NAME,U_AGE_Y,U_AGE_M,U_AGE_D,U_EMAIL,U_TEL1,U_TEL2,U_TEL3,U_ADDR1,U_ADDR2,U_ADDR3,U_GENDER,U_REGDATE) values ('iiiiii','iiiiii','아이아이','이아이',2003,12,9,'iiuiui@iiiiuu.com','010',7899,7899,'광주광역시','대구',' ','여성',to_date('22/09/29','RR/MM/DD'));
+Insert into USERTABLE (U_ID,U_PWD,U_NICKNAME,U_NAME,U_AGE_Y,U_AGE_M,U_AGE_D,U_EMAIL,U_TEL1,U_TEL2,U_TEL3,U_ADDR1,U_ADDR2,U_ADDR3,U_GENDER,U_REGDATE) values ('fafafa','ffffff','파파파파','김파파',1990,5,13,'fafa@fafa.com','010',4444,5555,'광주광역시','대구',' ','여성',to_date('22/09/29','RR/MM/DD'));
+Insert into USERTABLE (U_ID,U_PWD,U_NICKNAME,U_NAME,U_AGE_Y,U_AGE_M,U_AGE_D,U_EMAIL,U_TEL1,U_TEL2,U_TEL3,U_ADDR1,U_ADDR2,U_ADDR3,U_GENDER,U_REGDATE) values ('llllll','llllll','엘엘엘엘','이엘엘',2004,5,22,'lblalbla@lalb.com','010',1212,2323,'경상남도','대구',' ','여성',to_date('22/09/29','RR/MM/DD'));
+Insert into USERTABLE (U_ID,U_PWD,U_NICKNAME,U_NAME,U_AGE_Y,U_AGE_M,U_AGE_D,U_EMAIL,U_TEL1,U_TEL2,U_TEL3,U_ADDR1,U_ADDR2,U_ADDR3,U_GENDER,U_REGDATE) values ('hhhhhh','hhhhhh','에이치이','에이치',1985,8,5,'hahaha@aaa.com','010',7788,7788,'강원도','대구',' ','남성',to_date('22/09/29','RR/MM/DD'));
+
+Insert into NOTICETABLE (N_IDX,N_ID,N_NICKNAME,N_TITLE,N_CONTENT,N_DATE,N_COUNT) values (NOTICETABLE_SEQ.NEXTVAL,'system','관리자','첫번째 공지사항입니다.','첫번째 공지사항입니다.',to_date('22/09/28','RR/MM/DD'),2);
+Insert into NOTICETABLE (N_IDX,N_ID,N_NICKNAME,N_TITLE,N_CONTENT,N_DATE,N_COUNT) values (NOTICETABLE_SEQ.NEXTVAL,'system','관리자','두번째 공지사항입니다.','두번째 공지사항 입니다',to_date('22/09/29','RR/MM/DD'),1);
+Insert into NOTICETABLE (N_IDX,N_ID,N_NICKNAME,N_TITLE,N_CONTENT,N_DATE,N_COUNT) values (NOTICETABLE_SEQ.NEXTVAL,'system','관리자','세번째 공지사항입니다','세번째 공지사항입니다',to_date('22/09/29','RR/MM/DD'),0);
+
+
+Insert into BOARDTABLE (B_IDX,B_ID,B_NICKNAME,B_TITLE,B_CONTENT,B_DATE,B_COUNT,B_PWD,B_ADMINDEL) values (BOARDTABLE_SEQ.NEXTVAL,'aaaaaa','나는에이','안녕하세요','안녕하세요 에이입니다',to_date('22/09/28','RR/MM/DD'),1,'a',1);
+Insert into BOARDTABLE (B_IDX,B_ID,B_NICKNAME,B_TITLE,B_CONTENT,B_DATE,B_COUNT,B_PWD,B_ADMINDEL) values (BOARDTABLE_SEQ.NEXTVAL,'cccccc','씨씨는씨씨','씨입니다','이상해씨',to_date('22/09/29','RR/MM/DD'),0,'c',1);
+Insert into BOARDTABLE (B_IDX,B_ID,B_NICKNAME,B_TITLE,B_CONTENT,B_DATE,B_COUNT,B_PWD,B_ADMINDEL) values (BOARDTABLE_SEQ.NEXTVAL,'cccccc','씨씨는씨씨','좋은 밤입니다','모두 안녕하세요',to_date('22/09/29','RR/MM/DD'),2,'c',1);
+Insert into BOARDTABLE (B_IDX,B_ID,B_NICKNAME,B_TITLE,B_CONTENT,B_DATE,B_COUNT,B_PWD,B_ADMINDEL) values (BOARDTABLE_SEQ.NEXTVAL,'bbbbbb','나는비비','점심 맛있게 드세요','맛점맛점',to_date('22/09/29','RR/MM/DD'),1,'b',1);
+Insert into BOARDTABLE (B_IDX,B_ID,B_NICKNAME,B_TITLE,B_CONTENT,B_DATE,B_COUNT,B_PWD,B_ADMINDEL) values (BOARDTABLE_SEQ.NEXTVAL,'eeeeee','이에이승','철학책 추천 부탁드립니다','철학책 추천',to_date('22/09/29','RR/MM/DD'),2,'e',1);
+Insert into BOARDTABLE (B_IDX,B_ID,B_NICKNAME,B_TITLE,B_CONTENT,B_DATE,B_COUNT,B_PWD,B_ADMINDEL) values (BOARDTABLE_SEQ.NEXTVAL,'eeeeee','이에이승','쿠키 먹고싶다','초코쿠키',to_date('22/09/29','RR/MM/DD'),4,'e',1);
+Insert into BOARDTABLE (B_IDX,B_ID,B_NICKNAME,B_TITLE,B_CONTENT,B_DATE,B_COUNT,B_PWD,B_ADMINDEL) values (BOARDTABLE_SEQ.NEXTVAL,'wwwwww','더블유유','[관리자에 의해 삭제된 게시물입니다.]','비비는 바보멍청이
+[게시글 삭제 사유]
+											',to_date('22/09/29','RR/MM/DD'),1,'w',0);
+Insert into BOARDTABLE (B_IDX,B_ID,B_NICKNAME,B_TITLE,B_CONTENT,B_DATE,B_COUNT,B_PWD,B_ADMINDEL) values (BOARDTABLE_SEQ.NEXTVAL,'aaaaaa','나는에이','달이 떴습니다!!','마피아는 고개를 들어주세요!!',to_date('22/09/29','RR/MM/DD'),8,'a',1);
+
+Insert into JOINTABLE (J_IDX,J_ID,J_NICKNAME,J_CITY,J_LOCATION,J_HOBBYB,J_HOBBYS,J_TITLE,J_DATE,J_DDAY,J_MDAY,J_COUNT,J_COST,J_MAXMEM,J_NOWMEM,J_CONTENT,J_PWD,J_END,J_ADMINDEL) values (JOINTABLE_SEQ.NEXTVAL,'aaaaaa','나는에이','대전','중구','사진','필름','바다 사진 찍기',to_date('22/11/29','RR/MM/DD'),to_date('22/12/13','RR/MM/DD'),to_date('22/10/15','RR/MM/DD'),3,'n',3,1,'바다 사진','1',1,1);
+Insert into JOINTABLE (J_IDX,J_ID,J_NICKNAME,J_CITY,J_LOCATION,J_HOBBYB,J_HOBBYS,J_TITLE,J_DATE,J_DDAY,J_MDAY,J_COUNT,J_COST,J_MAXMEM,J_NOWMEM,J_CONTENT,J_PWD,J_END,J_ADMINDEL) values (JOINTABLE_SEQ.NEXTVAL,'bbbbbb','나는비비','서울','강남','오락','보드게임','보드게임 하실분',to_date('22/11/28','RR/MM/DD'),to_date('22/12/03','RR/MM/DD'),to_date('22/10/04','RR/MM/DD'),5,'n',6,2,'보드게임 합시다!!','b',1,1);
+Insert into JOINTABLE (J_IDX,J_ID,J_NICKNAME,J_CITY,J_LOCATION,J_HOBBYB,J_HOBBYS,J_TITLE,J_DATE,J_DDAY,J_MDAY,J_COUNT,J_COST,J_MAXMEM,J_NOWMEM,J_CONTENT,J_PWD,J_END,J_ADMINDEL) values (JOINTABLE_SEQ.NEXTVAL,'cccccc','씨씨는씨씨','부산','동구','언어','영어','영어회화, 영어로 토론하기',to_date('22/11/29','RR/MM/DD'),to_date('22/12/01','RR/MM/DD'),to_date('22/10/04','RR/MM/DD'),3,'n',4,1,'영어로 대화합시다','c',1,1);
+Insert into JOINTABLE (J_IDX,J_ID,J_NICKNAME,J_CITY,J_LOCATION,J_HOBBYB,J_HOBBYS,J_TITLE,J_DATE,J_DDAY,J_MDAY,J_COUNT,J_COST,J_MAXMEM,J_NOWMEM,J_CONTENT,J_PWD,J_END,J_ADMINDEL) values (JOINTABLE_SEQ.NEXTVAL,'aaaaaa','나는에이','부산','중구','공예','가죽공예','가죽공예 필통만들기',to_date('22/11/29','RR/MM/DD'),to_date('22/12/06','RR/MM/DD'),to_date('22/10/08','RR/MM/DD'),4,'Y',5,1,'필통만들기','q',1,1);
+
+Insert into BOARDCOMMENT (BC_IDX,BC_BIDX,BC_ID,BC_NICKNAME,BC_CONTENT,BC_DATE) values (BOARDCOMMENT_SEQ.NEXTVAL,8,'rrrrrr','알루미늄','마피아는 누구냐!',to_date('22/09/29','RR/MM/DD'));
+Insert into BOARDCOMMENT (BC_IDX,BC_BIDX,BC_ID,BC_NICKNAME,BC_CONTENT,BC_DATE) values (BOARDCOMMENT_SEQ.NEXTVAL,6,'rrrrrr','알루미늄','맛있는 쿠키',to_date('22/09/29','RR/MM/DD'));
+Insert into BOARDCOMMENT (BC_IDX,BC_BIDX,BC_ID,BC_NICKNAME,BC_CONTENT,BC_DATE) values (BOARDCOMMENT_SEQ.NEXTVAL,8,'hhhhhh','에이치이','탕탕!!',to_date('22/09/29','RR/MM/DD'));
+Insert into BOARDCOMMENT (BC_IDX,BC_BIDX,BC_ID,BC_NICKNAME,BC_CONTENT,BC_DATE) values (BOARDCOMMENT_SEQ.NEXTVAL,6,'hhhhhh','에이치이','아몬드쿠키도 맛있어요',to_date('22/09/29','RR/MM/DD'));
+Insert into BOARDCOMMENT (BC_IDX,BC_BIDX,BC_ID,BC_NICKNAME,BC_CONTENT,BC_DATE) values (BOARDCOMMENT_SEQ.NEXTVAL,5,'tttttt','티티티셔츠','철학 좋습니다',to_date('22/09/29','RR/MM/DD'));
+Insert into BOARDCOMMENT (BC_IDX,BC_BIDX,BC_ID,BC_NICKNAME,BC_CONTENT,BC_DATE) values (BOARDCOMMENT_SEQ.NEXTVAL,8,'tttttt','티티티셔츠','내가 마피아다!',to_date('22/09/29','RR/MM/DD'));
+
+Insert into JOINCOMMENT (JC_IDX,JC_BIDX,JC_ID,JC_NICKNAME,JC_CONTENT,JC_DATE) values (JOINCOMMENT_SEQ.NEXTVAL,3,'hhhhhh','에이치이','영어 회화 실력이 쑥쑥 늘겠네요.',to_date('22/11/30','RR/MM/DD'));
+Insert into JOINCOMMENT (JC_IDX,JC_BIDX,JC_ID,JC_NICKNAME,JC_CONTENT,JC_DATE) values (JOINCOMMENT_SEQ.NEXTVAL,2,'hhhhhh','에이치이','참여했습니다 루미큐브 한번 같이 하시죠',to_date('22/11/30','RR/MM/DD'));
+Insert into JOINCOMMENT (JC_IDX,JC_BIDX,JC_ID,JC_NICKNAME,JC_CONTENT,JC_DATE) values (JOINCOMMENT_SEQ.NEXTVAL,4,'tttttt','티티티셔츠','가죽공예 같이해요',to_date('22/11/30','RR/MM/DD'));
+
+
+COMMIT;
+
